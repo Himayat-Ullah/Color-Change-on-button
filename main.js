1ee3e8ad9ef3788bc1a5857f46b8c2339483579e
Himayat Ullah <himayat.ali01@gmail.com>
2022-05-14 12:55:13 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+function colorchange() {
+  para.style.color = "red";
+}
+function greenchange() {
+  para.style.color = "green";
+}
+function bluechange() {
+  para.style.color = "blue";
+}
+function yellowchange() {
+  para.style.color = "yellow";
+}
